489c7364f5b9f5786e1d72f02b728cb7dff4bd3c
cyhcyh2 <1619769726@qq.com>
2025-07-09 19:30:12 +0800

    一些更新（？

--- src/main/java/xyz/article/api/team/color/TeamColor.java
+++ src/main/java/xyz/article/api/team/color/TeamColor.java
@@ -1,28 +1,43 @@
 package xyz.article.api.team.color;
 
 public enum TeamColor {
-    RED,
-    BLUE,
-    GREEN,
-    YELLOW,
-    PURPLE,
-    CYAN ,
-    ORANGE,
-    PINK,
-    LIME,
-    TEAL,
-    BROWN,
-    GRAY,
-    BLACK,
-    WHITE,
-    EXTENDED1,
-    EXTENDED2,
-    EXTENDED3,
-    EXTENDED4,
-    EXTENDED5,
-    EXTENDED6,
-    EXTENDED7,
-    EXTENDED8,
-    EXTENDED9,
-    EXTENDED10;
+    RED ("red"),
+    BLUE ("blue"),
+    GREEN ("green"),
+    YELLOW ("yellow"),
+    PURPLE ("purple"),
+    CYAN ("cyan"),
+    ORANGE ("orange"),
+    PINK ("pink"),
+    LIME ("lime"),
+    TEAL ("teal"),
+    BROWN ("brown"),
+    GRAY ("gray"),
+    BLACK ("black"),
+    WHITE ("white"),
+    EXTENDED1 ("ex1"),
+    EXTENDED2 ("ex2"),
+    EXTENDED3 ("ex3"),
+    EXTENDED4 ("ex4"),
+    EXTENDED5 ("ex5"),
+    EXTENDED6 ("ex6"),
+    EXTENDED7 ("ex7"),
+    EXTENDED8 ("ex8"),
+    EXTENDED9 ("ex9"),
+    EXTENDED10 ("ex10");
+
+    private final String color;
+
+    TeamColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return color;
+    }
 }
